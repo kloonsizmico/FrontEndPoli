419a1366b1d0540f636a3fa77ad0579466d33961
Daniel Jimenez ! <wilmardaniel@outlook.com>
2019-12-21 21:30:09 -0500

    se agregó  data ui grid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AgGridModule } from 'ag-grid-angular';
 import { AppComponent } from './app.component';
 import { PersonasComponent } from './personas/personas.component';
 import { AppRoutingModule } from './app-routing.module';
 import { MenuComponent } from './menu/menu.component';
 import { PlanetasComponent } from './planetas/planetas.component';
+
+
 
 @NgModule({
   declarations: [
@@ -13,10 +15,13 @@ import { PlanetasComponent } from './planetas/planetas.component';
     PersonasComponent,
     MenuComponent,
     PlanetasComponent
+
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AgGridModule.withComponents([  ])
   ],
   providers: [],
   bootstrap: [AppComponent]
