5e147b0a82e0c9c2f1dfedf20ff146cc90460d5b
fcotabar <fcotabar@gmail.com>
2019-12-22 23:02:45 -0500

    Se implementó botón atrás en módulo

--- src/app/peliculas/peliculas.component.ts
+++ src/app/peliculas/peliculas.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-peliculas',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PeliculasComponent implements OnInit {
 
-  constructor() { }
+
+  constructor( private _location: Location ) {
+    }
 
   ngOnInit() {
+  }
+
+  back(){
+    this._location.back();
   }
 
 }
