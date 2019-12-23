1751af7fbe35fcb8df72ca28f54a68cf6986fc93
fcotabar <fcotabar@gmail.com>
2019-12-22 23:52:19 -0500

    Se implementó botón atrás en módulo vehículos

--- src/app/vehiculos/vehiculos.component.ts
+++ src/app/vehiculos/vehiculos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-vehiculos',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VehiculosComponent implements OnInit {
 
-  constructor() { }
+  constructor( private _location: Location ) { }
 
   ngOnInit() {
+  }
+
+  back(){
+    this._location.back();
   }
 
 }
