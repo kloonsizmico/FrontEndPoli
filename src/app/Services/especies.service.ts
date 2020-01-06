ebba97d7d9a64f34d518fc704aed50b2c8895538
fcotabar <fcotabar@gmail.com>
2020-01-06 13:08:34 -0500

    Se implementa y consume servicio especies

--- src/app/Services/especies.service.ts
+++ src/app/Services/especies.service.ts
@@ -1,10 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Especies } from '../interfaces/especies';
+// import { enviroment } from '../../environments/environment';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class EspeciesService {
 
-  constructor() { }
+  constructor( private httpClient: HttpClient) { }
 
+  getAllPeliculas(): Observable<Especies[]> {
+    return this.httpClient.get<Especies[]>( 'https://swapi.co/api/species/' );
+  }
 }
--- src/app/especies/especies.component.ts
+++ src/app/especies/especies.component.ts
@@ -1,5 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Location } from '@angular/common';
+import { Router } from '@angular/router';
+import { EspeciesService } from '../Services/especies.service';
+import { Especies } from '../interfaces/especies';
+import * as moment from 'moment';
+import { AgGridAngular } from 'ag-grid-angular';
+
 
 @Component({
   selector: 'app-especies',
@@ -8,9 +14,44 @@ import { Location } from '@angular/common';
 })
 export class EspeciesComponent implements OnInit {
 
-  constructor( private _location: Location ) { }
+  private defaultColDef;
+
+  constructor( private _location: Location, private router: Router, private especiesService: EspeciesService ) {
+    this.defaultColDef = { resizable: true, sortable: true, filter: true };
+  }
+
+  especies: Especies[];
+  rowData: any;
+
+  // @ts-ignore
+  @ViewChild('agGrid') agGrid: AgGridAngular;
+  columnDefs = [
+    {headerName: 'Nombre', width: 130,  field:  'name' },
+    {headerName: 'Especie', width: 100, field: 'classification'},
+    {headerName: 'Altura promedio', width: 140 , field: 'average_height' },
+    {headerName: 'Colores de cabello', width: 150 , field: 'hair_colors' },
+    {headerName: 'Colores de piel', width: 180 , field: 'skin_colors' },
+    {headerName: 'Colores de ojos', width: 160 , field: 'eye_colors' },
+    {headerName: 'Esperanza de vida', width: 150 , field: 'average_lifespan' },
+    {headerName: 'Idioma', width: 130, field: 'language' },
+    {headerName: 'Planeta', width: 210 , field: 'homeworld' },
+    {headerName: 'Creado', width: 150,  field: 'created', valueFormatter(params) {
+      return moment (params.value).format ('DD MMM, YYYY hh:mm'); }
+      },
+    {headerName: 'Editado', width: 150, field: 'edited',  valueFormatter(params) {
+      return moment (params.value).format ('DD MMM, YYYY hh:mm'); }
+      },
+    {headerName: 'Ubicación', width: 210, field: 'url' }
+  ];
 
   ngOnInit() {
+    this.getAllEspecies();
+  }
+
+  getAllEspecies() {
+    this.especiesService.getAllPeliculas().subscribe(resp => {
+      this.rowData = resp;
+    });
   }
 
   back(){
