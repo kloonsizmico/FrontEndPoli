77e8abd022cf7022398dc6e29d824fbe23437815
juankk81 <jcbo81@hotmail.com>
2020-01-09 21:47:36 -0500

    Implementacion servicio planetas

--- src/app/interfaces/vehiculos.ts
+++ src/app/interfaces/vehiculos.ts
@@ -1,17 +1,18 @@
 export interface Vehiculos {
-    name: string;
-    diameter: string;
-    orbital_period?: string;
-    rotation_period?: string;
-    gravity?: string;
-    population?: string;
-    climate?: string;
-    terrain?: string;
-    surface_water?: string;
-    residents?: string;
-    films?: string;
-    url?: string;
-    created?: string;
-    edited?: string;
-  }
-  
+ name: string;
+ model: string;
+ vehicle_class?: string;
+ manufacturer?: string;
+ length?: string;
+ cost_in_credits?: string;
+ crew?: string;
+ passengers?: string;
+ max_atmosphering_speed?: string;
+ cargo_capacity?: string;
+ consumables?: string;
+ pilots?: string;
+ films?: string;
+ url?: string;
+ created?: string;
+ edited?: string;
+}
--- src/app/vehiculos/vehiculos.component.ts
+++ src/app/vehiculos/vehiculos.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
+import { Router } from '@angular/router';
+import { VehiculosService } from '../Services/vehiculos.service';
+import { Vehiculos} from '../interfaces/vehiculos';
+import * as moment from 'moment';
+import { AgGridAngular } from 'ag-grid-angular';
+import { Vehiculos } from '../interfaces/vehiculos';
+
 
 @Component({
   selector: 'app-vehiculos',
@@ -8,7 +15,27 @@ import { Location } from '@angular/common';
 })
 export class VehiculosComponent implements OnInit {
 
-  constructor( private _location: Location ) { }
+  private defaultColDef;
+
+  constructor( private _location: Location, private router: Router, private planetasService: PlanetasService ) {
+    this.defaultColDef = { resizable: true, sortable: true, filter: true };
+  }
+  vehiculos: Vehiculos[];
+  rowData: any;
+
+  // @ts-ignore
+  @ViewChild('agGrid') agGrid: AgGridAngular;
+  columnDefs = [
+    {headerName: 'Nombre', width: 130,  field:  'name' },
+    {headerName: 'Diametro', width: 100, field: 'diameter'},
+    {headerName: 'Creado', width: 150,  field: 'created', valueFormatter(params) {
+      return moment (params.value).format ('DD MMM, YYYY hh:mm'); }
+      },
+    {headerName: 'Editado', width: 150, field: 'edited',  valueFormatter(params) {
+      return moment (params.value).format ('DD MMM, YYYY hh:mm'); }
+      },
+    {headerName: 'Ubicación', width: 210, field: 'url' }
+  ];
 
   ngOnInit() {
   }
